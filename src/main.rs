f4a5f839998a68c9757c2cb7fbc2712deecae52f
carterburn <899382+carterburn@users.noreply.github.com>
2026-04-09 21:52:39 -0400

    part1 blog work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,41 @@
-use foundrydb::add;
+use std::io::{self, stdin, BufRead, Write};
 
-fn main() {
-    println!("Inside fdb! {}", add(1, 2));
+fn main() -> Result<(), io::Error> {
+    let mut stdin = stdin().lock();
+    let mut buffer = String::new();
+
+    loop {
+        print!("fdb > ");
+        io::stdout().flush()?;
+
+        buffer.clear();
+
+        match stdin.read_line(&mut buffer) {
+            Ok(0) => {
+                // EOF; just return (no error)
+                return Ok(());
+            }
+            Ok(_n) => {
+                process_command(buffer.trim());
+            }
+            Err(e) => {
+                eprintln!("Error while reading input: {e:?}");
+                return Err(e);
+            }
+        }
+    }
+}
+
+fn process_command(cmd: &str) {
+    let cmd = cmd.to_lowercase();
+    match cmd.as_str() {
+        ".exit" => {
+            // Exit with success
+            eprintln!("Goodbye!");
+            std::process::exit(0);
+        }
+        _ => {
+            println!("Unrecognized command: {cmd}");
+        }
+    }
 }
